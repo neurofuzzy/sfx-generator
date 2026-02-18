df202869407db6afc960b74912287c7825e58390
Geoff Gaudreault <gaudreault@gmail.com>
2026-02-18 05:16:54 +0000

    it just deployed the README https://neurofuzzy.github.io/sfx-generator/

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,8 @@ const nextConfig: NextConfig = {
      - Locally or in Studio, use '' (root)
   */
   basePath: process.env.GITHUB_ACTIONS === 'true' ? '/sfx-generator' : '',
+  /* trailingSlash: true ensures that folders are generated with index.html, which is better for GH Pages */
+  trailingSlash: true,
   /* Images must be unoptimized for static hosting */
   images: {
     unoptimized: true,
